3bdb4bf42fbfd7f7ec21a7d65f453fcb9802b14f
SHIKHAR GUPTA <132701737+ShikharGupta0813@users.noreply.github.com>
2025-01-06 14:07:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+struct Item {
+   int value;
+   int weight;
+};
+class Solution {
+   public:
+      bool static comp(Item a, Item b) {
+         double r1 = (double) a.value / (double) a.weight;
+         double r2 = (double) b.value / (double) b.weight;
+         return r1 > r2;
+      }
+   // function to return fractionalweights
+   double fractionalKnapsack(vector<int>v,vector<int>w, int W) {
+      int n = v.size();
+      vector<Item>arr(n,{-1,-1});
+      
+      for(int i=0;i<v.size();i++){
+          arr[i].value=v[i];
+          arr[i].weight=w[i];
+      }
+      
+      sort(arr.begin(), arr.end(), comp);
+
+      int curWeight = 0;
+      double finalvalue = 0.0;
+
+      for (int i = 0; i < arr.size(); i++) {
+
+         if (curWeight + arr[i].weight <= W) {
+            curWeight += arr[i].weight;
+            finalvalue += arr[i].value;
+         } else {
+            int remain = W - curWeight;
+            finalvalue += (arr[i].value / (double) arr[i].weight) * (double) remain;
+            break;
+         }
+      }
+
+      return finalvalue;
+
+   }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    // taking testcases
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the number of test cases
+    cout << setprecision(6) << fixed;
+
+    while (t--) {
+        // Reading the value array
+        vector<int> values;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            values.push_back(number);
+        }
+
+        // Reading the weight array
+        vector<int> weights;
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            weights.push_back(number);
+        }
+
+        // Reading the capacity
+        int w;
+        cin >> w;
+        cin.ignore(); // to ignore the newline after capacity
+
+        // function call
+        Solution ob;
+        cout << ob.fractionalKnapsack(values, weights, w) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
