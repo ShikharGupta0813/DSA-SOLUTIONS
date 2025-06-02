dc0045d979d3c1ccdc6cbf8fa24146ba4db5149e
SHIKHAR GUPTA <132701737+ShikharGupta0813@users.noreply.github.com>
2025-06-02 17:27:55 +0530

    Time: 0 ms (100%), Space: 9.4 MB (9.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,12 +4,32 @@ public:
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return helper(m-1,n-1,m,n,dp);
     }
-    int helper(int i,int j,int n,int m,vector<vector<int>>dp){
+    int helper(int i,int j,int n,int m,vector<vector<int>>&dp){
         if(i==0&&j==0) return 1;
-        if(i<0||i>n-1||j<0||j>m-1) return 0;
+        if(i<0||j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int up = helper(i-1,j,n,m,dp);
         int left =helper(i,j-1,n,m,dp);
         return dp[i][j] =up + left;
     }
 };
+
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>>dp(m,vector<int>(n,-1));
+//         return helper(m-1,n-1,m,n,dp);
+//     }
+//     int helper(int i,int j,int n,int m,vector<vector<int>>dp){
+//         if(i==0&&j==0) return 1;
+//         dp[0][0]=1;
+//         for(int i=1;i<n;i++){
+//             for(int j=1;j<m;j++){
+//              int up = dp[i-1][j]
+//              int left = dp[i][j-1]
+//             }
+//         }
+       
+//         return dp[n-1][m-1] 
+//     }
+// };
