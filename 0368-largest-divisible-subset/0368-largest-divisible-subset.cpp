e54e2e6789bb4cefcfb2dcffe219e30fe92b65dd
SHIKHAR GUPTA <132701737+ShikharGupta0813@users.noreply.github.com>
2025-06-24 13:49:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -11,14 +11,15 @@ public:
 
         int maxi = 1;
         int lastind = 0;
-
+        int prev=0;
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    hash[i] = j;
+            // for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[prev] == 0 && dp[prev] + 1 > dp[i]) {
+                    dp[i] = dp[prev] + 1;
+                    hash[i] = prev;
+                    prev=i;
                 }
-            }
+            
             if (dp[i] > maxi) {
                 maxi = dp[i];
                 lastind = i;
